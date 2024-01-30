03d624d35b47b03c87a47da0fcda5daacd05b936
Shahzaib <szaib1386@gmail.com>
2024-01-30 13:32:30 +0500

    "add" rating and fix twice rendering

--- src/components/movie/Right_MS_Details.tsx
+++ src/components/movie/Right_MS_Details.tsx
@@ -1,17 +1,16 @@
 import { Params, useParams } from "react-router-dom";
 import { LuPlus } from "react-icons/lu";
+import { RxSlash } from "react-icons/rx";
 import { useGetInfoQuery } from "@/redux/rtk_query/api";
 import { TypographyP } from "../ui/Typography/TypographyP";
 import { TypographyH2 } from "../ui/Typography/TypographyH2";
 import AnimateTitle from "../ui/Typography/AnimateTitle";
-import { Button } from "../ui/button";
-import { useAddToFavoriteList } from "@/hooks/addToFavoriteList";
+import AddToFavList from "./AddToFavListBtn";
 
 function Right_MS_Details({ className }: { className?: string }) {
+  console.log("right side detailssssss");
   const { media_type, id } = useParams<Params>();
   const { data: movieData } = useGetInfoQuery(`${media_type}/${id}`);
-  const { addToFavoriteList } = useAddToFavoriteList();
-
   const year =
     movieData?.release_date?.split("-")[0] ||
     movieData?.first_air_date?.split("-")[0];
@@ -25,7 +24,6 @@ function Right_MS_Details({ className }: { className?: string }) {
     title: String(title),
     poster_path: String(movieData?.backdrop_path),
   };
-
   return (
     <div id="rightDetails" className={`p-1 ${className}`}>
       <TypographyH2 className="border-none space-x-1 pb-[0_!important]">
@@ -33,18 +31,18 @@ function Right_MS_Details({ className }: { className?: string }) {
         <AnimateTitle text={shortTitle} className="@xl:hidden inline-block" />
         {year && <span className="text-[0.7em] text-gray-500">({year})</span>}
       </TypographyH2>
-      <div>
-        <TypographyP className="mb-3 text-lg leading-[1] italic text-gray-400">
-          {movieData?.tagline}
-        </TypographyP>
-      </div>
-      <Button
-        variant={"destructive"}
-        onClick={() => addToFavoriteList(favData)}
-      >
+      <TypographyP className="mb-3 text-lg leading-[1] italic text-gray-400">
+        {movieData?.tagline}
+      </TypographyP>
+      <TypographyP className="mb-3 text-lg leading-[1] italic">
+        <span className="">{movieData?.vote_average.toFixed(1)}</span>
+        <span className="text-2xl text-gray-400">/</span>
+        <span className="text-gray-400">10</span>
+      </TypographyP>
+      <AddToFavList className="" variant="destructive" data={favData}>
         <LuPlus />
         Add List
-      </Button>
+      </AddToFavList>
     </div>
   );
 }
